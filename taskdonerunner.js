b58ab0cf461e5f1ee48d121e74f6ea40411d7917
Beameee <afiq.muzakkir09@gmail.com>
2026-06-21 10:25:27 +0800

    mock database and button event listener

---
+++ taskdonerunner.js
@@ -0,0 +1,25 @@
+
+const MockDatabaseResponse =
+{
+    taskName: "Beli Barang",
+    taskRunner: "Bangcok",
+    taskDuration: "60 minutes",
+    taskRating: "5",
+    taskPrice: "RM20.00"
+}
+
+function LoadReceiptData(taskDetails)
+{
+    document.getElementById('taskName').textContent = taskDetails.taskName;
+    document.getElementById('taskRunner').textContent = taskDetails.taskRunner;
+    document.getElementById('taskDuration').textContent = taskDetails.taskDuration;
+    document.getElementById('taskPrice').textContent = taskDetails.taskPrice;
+    document.getElementById('taskRating').textContent = taskDetails.taskRating;
+}
+
+LoadReceiptData(MockDatabaseResponse);
+
+document.getElementById('browseMoreBtn').addEventListener('click', () =>
+{
+    window.location.href = 'browseTasks.html';
+});
